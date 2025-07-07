b142ad2dff1f5ce1c405a8505a8df12d38420851
tacky-and-me <tfyoroyoro@gmail.com>
2025-07-07 09:37:30 +0900

    aaa

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ public class Main {
   public static void main(String[] args) {
     System.out.println("SEMonster!");
     Player player = new Player();
-    drawMonsters();
-    showDeck();
+    player.drawMonsters();
+    player.showDeck();
   }
 }
